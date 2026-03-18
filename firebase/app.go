d3c28b9cf86a0dd0eafa977ee4026c4510696a3a
Ramy Kaddouri <myramkadd@gmail.com>
2026-03-18 18:16:33 -0400

    fix firebase init

--- firebase/app.go
+++ firebase/app.go
@@ -6,18 +6,14 @@ import (
 	"os"
 
 	firebase "firebase.google.com/go/v4"
-	"google.golang.org/api/option"
 )
 
 func GetFirebaseApp(useEmulators bool) (*firebase.App, error) {
-	var opt option.ClientOption = nil
-
 	if os.Getenv("PROJECT_ID") == "" {
 		log.Fatalf("PROJECT_ID not found in env")
 	}
 
 	if useEmulators {
-		opt = option.WithoutAuthentication()
 		os.Setenv("FIRESTORE_EMULATOR_HOST", "localhost:8080")
 	}
 
@@ -25,7 +21,7 @@ func GetFirebaseApp(useEmulators bool) (*firebase.App, error) {
 		ProjectID: os.Getenv("PROJECT_ID"),
 	}
 
-	app, err := firebase.NewApp(context.Background(), &cfg, opt)
+	app, err := firebase.NewApp(context.Background(), &cfg)
 	if err != nil {
 		log.Fatalf("Failed to init firebase app: %v", err)
 	}
