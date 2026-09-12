f09222d19e8120474ed8c3b351190cf92945a370
Lance <lanceu0128@gmail.com>
2026-09-12 14:50:39 -0700

    move queue out of main infra, minor changes

--- infra/main.tf
+++ infra/main.tf
@@ -5,14 +5,6 @@ terraform {
     }
 }
 
-resource "google_service_account" "queue_invoker" {
-  account_id      = "queue-invoker"
-  deletion_policy = "ABANDON"
-  description     = "Service account used by Cloud Tasks to invoke Cloud Run services"
-  display_name    = "Cloud Tasks Queue Invoker"
-  project         = "h4i-applications"
-}
-
 resource "google_artifact_registry_repository" "professor" {
   cleanup_policy_dry_run = false
   deletion_policy        = "ABANDON"
@@ -43,26 +35,6 @@ resource "google_artifact_registry_repository" "professor" {
 
   vulnerability_scanning_config {
     enablement_config = "DISABLED"
-  }
-}
-
-resource "google_cloud_tasks_queue" "professor_grading_requests" {
-  deletion_policy = "ABANDON"
-  desired_state   = "RUNNING"
-  location        = "us-east4"
-  name            = "professor-grading-requests"
-  project         = "h4i-applications"
-
-  rate_limits {
-    max_concurrent_dispatches = 10
-    max_dispatches_per_second = 500
-  }
-
-  retry_config {
-    max_attempts  = 5
-    max_backoff   = "3600s"
-    max_doublings = 16
-    min_backoff   = "0.100s"
   }
 }
 
@@ -120,12 +92,4 @@ resource "google_project_iam_member" "github_actions_service_account_user" {
   project = "h4i-applications"
   role    = "roles/iam.serviceAccountUser"
   member  = "serviceAccount:github-actions@h4i-applications.iam.gserviceaccount.com"
-}
-
-resource "google_cloud_run_service_iam_member" "professor_queue_invoker" {
-  project  = "h4i-applications"
-  location = "us-east4"
-  service  = "professor-service"
-  role     = "roles/run.invoker"
-  member   = "serviceAccount:queue-invoker@h4i-applications.iam.gserviceaccount.com"
 }
