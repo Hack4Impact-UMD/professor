43315de145852a81793fc1b2cfba28c0c03fb2b3
Lance <lanceu0128@gmail.com>
2026-09-12 18:56:32 -0700

    add github actions account to terraform

--- infra/actions.tf
+++ infra/actions.tf
@@ -1,6 +1,12 @@
 locals {
-  github_actions_service_account_email = "github-actions@${local.project_id}.iam.gserviceaccount.com"
-  github_actions_repo_principal        = "principalSet://iam.googleapis.com/projects/${local.project_number}/locations/global/workloadIdentityPools/github/attribute.repository/${local.github_repository_owner}/${local.github_repository_name}"
+  github_actions_repo_principal = "principalSet://iam.googleapis.com/projects/${local.project_number}/locations/global/workloadIdentityPools/github/attribute.repository/${local.github_repository_owner}/${local.github_repository_name}"
+}
+
+resource "google_service_account" "github_actions" {
+  account_id      = "github-actions"
+  deletion_policy = "ABANDON"
+  display_name    = "GitHub Actions"
+  project         = local.project_id
 }
 
 resource "google_iam_workload_identity_pool" "github" {
@@ -30,13 +36,13 @@ resource "google_iam_workload_identity_pool_provider" "github" {
 }
 
 resource "google_service_account_iam_member" "github_actions_wif_user" {
-  service_account_id = "projects/${local.project_id}/serviceAccounts/${local.github_actions_service_account_email}"
+  service_account_id = "projects/${local.project_id}/serviceAccounts/${google_service_account.github_actions.email}"
   role               = "roles/iam.workloadIdentityUser"
   member             = local.github_actions_repo_principal
 }
 
 resource "google_service_account_iam_member" "github_actions_token_creator" {
-  service_account_id = "projects/${local.project_id}/serviceAccounts/${local.github_actions_service_account_email}"
+  service_account_id = "projects/${local.project_id}/serviceAccounts/${google_service_account.github_actions.email}"
   role               = "roles/iam.serviceAccountTokenCreator"
   member             = local.github_actions_repo_principal
 }
@@ -44,7 +50,7 @@ resource "google_service_account_iam_member" "github_actions_token_creator" {
 resource "google_project_iam_member" "github_actions_artifact_registry_writer" {
   project = local.project_id
   role    = "roles/artifactregistry.writer"
-  member  = "serviceAccount:${local.github_actions_service_account_email}"
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
 resource "google_cloud_run_v2_service_iam_member" "github_actions_run_admin" {
@@ -52,11 +58,11 @@ resource "google_cloud_run_v2_service_iam_member" "github_actions_run_admin" {
   location = local.region
   name     = google_cloud_run_v2_service.professor.name
   role    = "roles/run.admin"
-  member  = "serviceAccount:${local.github_actions_service_account_email}"
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
 resource "google_service_account_iam_member" "github_actions_service_account_user" {
   service_account_id = google_service_account.professor_service.name
   role    = "roles/iam.serviceAccountUser"
-  member  = "serviceAccount:${local.github_actions_service_account_email}"
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
 }
